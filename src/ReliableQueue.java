1c9b0fba30b18fab9626c619cebeacbe2e35177a
Siuujal_3 <sujalbkkashid@gmail.com>
2026-06-22 11:12:19 +0530

    feat: Implement DeadLetterQueue, max-retries in Reliable-Queue

--- src/ReliableQueue.java
+++ src/ReliableQueue.java
@@ -2,11 +2,15 @@ import java.util.UUID;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Optional;
+import java.time.Instant;
 
 public class ReliableQueue
 {
     private final MessageQueue<MessageDelivery> availableQueue;
     private final Map<UUID, MessageDelivery> inFlightMessages;
+    private final MessageQueue<MessageDelivery> deadLetterQueue;
+    private final int maxRetries;
+    
 
     public ReliableQueue(int capacity)
     {
@@ -15,7 +19,10 @@ public class ReliableQueue
             throw new IllegalArgumentException("Capacity cannot be <= 0");
         }
         this.availableQueue = new MessageQueue<>(5);
+        this.deadLetterQueue = new MessageQueue<>(5);
         this.inFlightMessages = new HashMap<>();
+
+        this.maxRetries = 3;
     }
 
     public boolean publish(Message message)
@@ -24,23 +31,32 @@ public class ReliableQueue
         {
             throw new IllegalArgumentException("Message cannot be null");
         }
-        MessageDelivery messageDelivery = new MessageDelivery(message);
+        MessageDelivery messageDelivery = new MessageDelivery(message, 0);
         return availableQueue.offer(messageDelivery);
     }
 
     public Optional<Message> receive()
     {
-        MessageDelivery messageDelivery = availableQueue.poll();
+        MessageDelivery oldMessageDelivery = availableQueue.poll();
 
-        if(messageDelivery == null)
+        if(oldMessageDelivery == null)
         {
             return Optional.empty();
         }
 
-        Message message = messageDelivery.getMessage();
+        Message message = oldMessageDelivery.getMessage();
         UUID id = message.getId();
+        int retryCount = oldMessageDelivery.getRetryCount();
 
-        inFlightMessages.put(id, messageDelivery);
+        if(retryCount > maxRetries)
+        {
+            deadLetterQueue.offer(oldMessageDelivery);
+            return Optional.empty();
+        }
+
+        MessageDelivery newMessageDelivery = new MessageDelivery(message, Instant.now(), retryCount + 1);
+
+        inFlightMessages.put(id, newMessageDelivery);
         
         return Optional.ofNullable(message);
 
